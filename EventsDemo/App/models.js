144b920c27fb450208a0a5e381f885d30fe26e62
Ramanajee Kandregula <ramanajee516@live.in>
2015-10-15 00:47:06 +0530

    HandlerbarJS

---
+++ EventsDemo/App/models.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="c:\users\ram\documents\visual studio 2015\Projects\EventsDemo\Scripts/backbone.js" />
+
+
+var EventModel = Backbone.Model.extend({
+    urlRoot: "DAL/data.json",
+    initialize: function () {
+    }
+});
---
+++ EventsDemo/App/views.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="c:\users\ram\documents\visual studio 2015\Projects\EventsDemo\Scripts/backbone.js" />
+
+var EventView = Backbone.View.extend({
+    el: '#resultDiv',
+    initialize: function () {
+
+    },
+    render: function () {
+        var source = $("#events_template").html();
+        var template = Handlebars.compile(source);
+        var eventModel = new EventModel();
+        var self = this;
+        eventModel.fetch({
+            success: function (response) {
+                var data = response.toJSON();
+                _.each(data, function (event, index) {
+                    var html = template(event);
+                    self.$el.append(html)
+                });
+            }
+        });
+        return this;
+    },
+    eventDetails: function () {
+        var source = $("#event_details").html();
+        var template = Handlebars.compile(source);
+        var eventModel = new EventModel();
+        var self = this;
+        eventModel.fetch({
+            success: function (response) {
+                var data = response.toJSON();
+                _.each(data, function (event, index) {
+                    var html = template(event);
+                    self.$el.append(html)
+                })
+            }
+        })
+    }
+});
